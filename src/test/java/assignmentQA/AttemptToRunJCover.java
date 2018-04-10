06d2e41b8a8b7ebd4a951dcc1daf80a5a671f07a
Daniel Di Nardo <daniel.dinardo@uni.lu>
2018-04-10 23:03:38 +0200

    Did a bit more work on the code coverage implementation.

--- src/test/java/assignmentQA/AttemptToRunJCover.java
+++ src/test/java/assignmentQA/AttemptToRunJCover.java
@@ -2,7 +2,10 @@ package assignmentQA;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -12,7 +15,9 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
@@ -43,26 +48,7 @@ public class AttemptToRunJCover {
             "--report-dir=" + reportDir
     };
 
-    private final WebDriver webClient = getWebClient();	
-	
-    private WebDriver getWebClient() {
-        Proxy proxy = new Proxy().setHttpProxy("localhost:3129");
-        DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(CapabilityType.PROXY, proxy);
-        
-		System.setProperty("webdriver.chrome.driver", "/Users/ddinardo/Documents/ChromeDriver/chromedriver");
-		
-		ChromeOptions chromeOptions = new ChromeOptions();
-
-		chromeOptions.addArguments("window-size="+this.screenWidth+","+this.screenHeight);
-		chromeOptions.addArguments("disable-infobars");
-		//chromeOptions.addArguments("headless");
-		chromeOptions.addArguments("proxy-server="+"localhost:3129");
-		
-		return new ChromeDriver(chromeOptions);
-        
-        //return new ChromeDriver(cap);
-    }
+    @Rule public TestName name = new TestName();
 	
     @BeforeClass
     public static void setUpOnce() {
@@ -80,8 +66,7 @@ public class AttemptToRunJCover {
         main.stop();
     }
 	
-	
- 	//private WebDriver driver;
+ 	private WebDriver driver;
 	private Integer screenWidth;
 	private Integer screenHeight;
 	private Boolean recordCoverage;
@@ -91,7 +76,7 @@ public class AttemptToRunJCover {
 		// screen width (pixels), screen height (pixels), recordCoverage
 		return Arrays.asList(new Object[][] {
 			{ 1366, 768, false },
-			{ 400, 300, false }
+			{ 1366, 768, true },
 		});
 	}
 	
@@ -101,52 +86,67 @@ public class AttemptToRunJCover {
 		this.recordCoverage = recordCoverage;		
 	}
 	
-    @Before
-    public void setUp2() {
-		InitializeEnvironment.initializeUsingREST(10);
-        File jsonFile = new File(reportDir+"/jscoverage.json");
-        if (jsonFile.exists())
-            jsonFile.delete();
-        webClient.get(InitializeEnvironment.getWebAppLocation());
-    }
-	
 	@Before
 	public void setUp() throws Exception {
-		//System.setProperty("webdriver.chrome.driver", "/Users/ddinardo/Documents/ChromeDriver/chromedriver");
+		InitializeEnvironment.configureChromeDriver();
+		InitializeEnvironment.initializeUsingREST(10);
 		
-		//ChromeOptions chromeOptions = new ChromeOptions();
-
-		//chromeOptions.addArguments("window-size="+this.screenWidth+","+this.screenHeight);
-		//chromeOptions.addArguments("disable-infobars");
-		//chromeOptions.addArguments("headless");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("window-size="+this.screenWidth+","+this.screenHeight);
+		chromeOptions.addArguments("disable-infobars");
+		if (this.recordCoverage == true) {
+			chromeOptions.addArguments("proxy-server="+"localhost:3129");
+		}
 		
-		//webClient = new ChromeDriver(chromeOptions);
-		//webClient.get("http://localhost:3000/");
-	}
+		driver = new ChromeDriver(chromeOptions);
+		driver.get(InitializeEnvironment.getWebAppLocation());
+	}	
+	
+	void writeToFile(String fileName, String json) {
+		BufferedWriter bw = null;
+		FileWriter fw = null;
 
-	@After
-	public void tearDown() throws Exception {
-		//webClient.quit();
+		try {
+			fw = new FileWriter(fileName, false);
+			bw = new BufferedWriter(fw);
+			bw.write(json);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+				if (fw != null) {
+					fw.close();
+				}
+			} 
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 	
-    @After
-    public void tearDown2() {
-        try {
-            webClient.close();
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-        try {
-            webClient.quit();
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-    }
+	@After
+	public void tearDown() throws Exception {
+		if (this.recordCoverage == true) {
+			// save coverage results to file
+			String json = (String)((JavascriptExecutor) driver).executeScript("return jscoverage_serializeCoverageToJSON();");
+			Class clazz = this.getClass();
+			String className = clazz.getCanonicalName();
+			String methodName = name.getMethodName();
+			writeToFile(reportDir + "/" + className + "." + methodName + ".json", json);
+		}
+
+		driver.quit();
+	}
 
 	@Test
 	public void testDateFormat() {
     		// should rather use a unique id
-    		WebElement bannerDateElement = webClient.findElement(By.xpath("//span[@class='banner-date']/div"));
+    		WebElement bannerDateElement = driver.findElement(By.xpath("//span[@class='banner-date']/div"));
     		String bannerDate = bannerDateElement.getText();
     		String systemDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-YYYY")).toString();
     		
@@ -156,13 +156,12 @@ public class AttemptToRunJCover {
 	@Test
 	public void testBannerIsBlack() {
     		// is the grey value returned actually acceptable? Maybe need to refine test plan.
-    		WebElement assignmentMastHead = webClient.findElement(By.xpath("//div[@class='assignment-masthead']"));
+    		WebElement assignmentMastHead = driver.findElement(By.xpath("//div[@class='assignment-masthead']"));
     		String backgroundColor = assignmentMastHead.getCssValue("background-color");
     		String hexBackgroundColor = Color.fromString(backgroundColor).asHex();
     		String hexExpectedColor = "#000000"; // the color black
     		
     		assertEquals(hexExpectedColor, hexBackgroundColor);
 	}
-
 
 }
